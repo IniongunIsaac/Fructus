0b53a6acd6175cd7de20c8e40b689df70ea6ac22
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-21 15:04:09 +0100

    fruits list

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var fruits: [Fruit] = fruitsData
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            List(fruits.shuffled()) { fruit in
+                FruitRowView(fruit: fruit)
+                    .padding(.vertical, 4)
+            } //: List
+            .navigationTitle("Fruits")
+        } //: NavigationView
     }
 }
 
